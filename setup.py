c576e5a228a26507a9c57a2c543182b58035dae9
Inbar Rose <5904674+InbarRose@users.noreply.github.com>
2020-03-28 12:09:02 +0300

    Bump version: 1.0.9 → 1.0.10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
 
 setuptools.setup(
     name="kitir",
-    version="1.0.9",
+    version="1.0.10",
     author="Inbar Rose",
     author_email="inbar.rose1@gmail.com",
     python_requires=">=3.7.4",
